e1305946c5a3b9d64b887605b7108713b6fe9917
2504-tulika <tulika2504@gmail.com>
2026-03-20 17:30:44 +0530

    fun2: Implemented average calculation function

--- Tulika_frontend/JS/tulika_student_analyzer.js
+++ Tulika_frontend/JS/tulika_student_analyzer.js
@@ -38,7 +38,7 @@ const students = [
 
 // console.log("Student Data Loaded Successfully");
 
-// Function to calculate total marks of a student
+//Function to calculate total marks of a student
 function calTotal(student) {
   let total = 0;
 
@@ -54,4 +54,22 @@ console.log("\nTotal Marks of Each Student:");
 for (let i = 0; i < students.length; i++) {
   let totalMarks = calTotal(students[i]);
   console.log(students[i].name + " Total Marks: " + totalMarks);
+}
+
+// Function to calculate average marks of one student
+
+function calAverage(student) {
+  let totalMarks = calTotal(student);
+  let numberOfSubjects = student.marks.length;
+
+  let average = totalMarks / numberOfSubjects;
+
+  return average;
+}
+// Iterating through each student data and calculating average marks
+console.log("\nAverage Marks of Each Student:");
+
+for (let i = 0; i < students.length; i++) {
+  let averageMarks = calAverage(students[i]);
+  console.log(students[i].name + " Average: " + averageMarks.toFixed(2));
 }
