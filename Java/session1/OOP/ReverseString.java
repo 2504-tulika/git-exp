217e21df7aaed5693f4965cde648801e3df2cd43
2504-tulika <tulika2504@gmail.com>
2026-04-08 18:11:41 +0530

    VowelCounter and Reverse String programs

---
+++ Java/session1/OOP/ReverseString.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+    // Method to reverse string
+    public static String reverse(String str) {
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String result = reverse(input);
+
+        System.out.println("Reversed String: " + result);
+
+        sc.close();
+    }
+}
