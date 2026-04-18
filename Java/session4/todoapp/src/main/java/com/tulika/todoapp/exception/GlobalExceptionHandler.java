f1965e0859b8c0bd4da679166a7e175a04de2d05
2504-tulika <tulika2504@gmail.com>
2026-04-19 00:23:44 +0530

    Added global exception handler

---
+++ Java/session4/todoapp/src/main/java/com/tulika/todoapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.tulika.todoapp.exception;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
