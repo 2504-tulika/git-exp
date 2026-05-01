31e439a3e74d3eb69391a47f3adef99d0cdad86f
Tulika Lunkad <145529916+2504-tulika@users.noreply.github.com>
2026-05-01 14:42:42 +0530

    Delete Capstone_SRSTicketbooking directory

--- Capstone_SRSTicketbooking/backend/event-booking/src/main/java/com/tulika/eventbooking/exception/BookingException.java
+++
@@ -1,8 +0,0 @@
-package com.tulika.eventbooking.exception;
-
-public class BookingException extends RuntimeException {
-
-    public BookingException(String message) {
-        super(message);
-    }
-}
--- Capstone_SRSTicketbooking/backend/event-booking/src/main/java/com/tulika/eventbooking/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.tulika.eventbooking.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
