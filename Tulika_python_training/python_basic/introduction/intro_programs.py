f5a01afb1af66a4161c6ed6133a6b09e304b2fa9
Tulika <tulika2504@gmail.com>
2026-06-09 22:44:26 +0530

    feat: add introductory python programs

---
+++ Tulika_python_training/python_basic/introduction/intro_programs.py
@@ -0,0 +1,19 @@
+"""
+Basic introductory Python programs.
+"""
+
+import sys
+
+
+# Question 1
+print("Welcome to Python Training")
+
+# Question 2
+print("Current Python Version:")
+print(sys.version)
+
+# Question 3
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+print(f"Hello {user_name}! You are {user_age} years old.")
